dd3acb1da90a584c0d065cabe37771b7e9bbb404
Charlotte Studio <charlottexprojects@gmail.com>
2023-06-01 03:09:42 +0800

    add BLE Client Set up

--- SoilSensorDevice/BLE_Client_Setup_Header.h
+++ SoilSensorDevice/BLE_Client_Setup_Header.h
@@ -1,9 +1,25 @@
 #include "BLE_Client_Header.h"
 
+#define maxCharacteristicUUIDChannelCount maxCharacteristicCount-2
+
 static char* serviceUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-static char* characteristicUUID_SoilSensor = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-static char* characteristicUUID_WaterPump  = "beb5483e-36e1-4688-b7f5-ea07361b26a7";
 static char* deviceName = "Soil_Sensor";
+
+static char* characteristicUUID_To       = "beb5483e-36e1-4688-b7f5-ea00361b26a0";  // for check target
+static char* characteristicUUID_Message  = "beb5483e-36e1-4688-b7f5-ea00361b26a1";  // for check json
+
+static char* characteristicUUID_channel[maxCharacteristicUUIDChannelCount] = {
+  "beb5483e-36e1-4688-b7f5-ea10121b26a1",
+  "beb5483e-36e1-4688-b7f5-ea10121b26a2",
+  "beb5483e-36e1-4688-b7f5-ea10121b26a3",
+  "beb5483e-36e1-4688-b7f5-ea10121b26a4",
+  "beb5483e-36e1-4688-b7f5-ea10121b26a5",
+  "beb5483e-36e1-4688-b7f5-ea10121b26a6",
+};
+
+static String ble_empty   = "_";
+static String ble_to      = "_";
+static String ble_message = "_";
 
 #define scanDuration 3               // scan BLE time
 #define waitingTime_bleScaner 5000   // scan BLE and try connect will use this time
@@ -13,7 +29,13 @@ static unsigned long nextTime_bleScaner = 0;
 void SetUpBLE()
 {
   SetUpBLEDevices(deviceName);
-  RegisterCharacteristic(characteristicUUID_SoilSensor);
+  RegisterCharacteristic(characteristicUUID_To);
+  RegisterCharacteristic(characteristicUUID_Message);
+
+  for(int i = 0; i < maxCharacteristicUUIDChannelCount; i++)
+  {
+    RegisterCharacteristic(characteristicUUID_channel[i]);
+  }
 }
 
 void ScanBLEAndConnect()
